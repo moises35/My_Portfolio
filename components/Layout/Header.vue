8e0af8dd1f0e58d8bc9edf65d37ab0e3819d7691
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-12-18 17:39:18 -0300

    Added: header component

---
+++ components/Layout/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="header">
+    <div class="container layout-wrapper">
+      <div class="logo">
+        <NuxtLink to="/">
+          <SVGLogo v-if="colorMode.preference === 'dark'" />
+          <SVGLogoLight v-else />
+        </NuxtLink>
+      </div>
+      <LayoutNav />
+      <div class="switchers">
+        <AtomSwitcherColorMode />
+        <AtomSwitcherLanguage />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import SVGLogo from '@/assets/svg/logo.svg'
+import SVGLogoLight from '@/assets/svg/logo-light.svg'
+
+const colorMode = useColorMode()
+</script>
+
+<style lang="scss">
+.header {
+  width: 100%;
+  background-color: var(--bg-secondary);
+
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 80px;
+
+    .logo {
+      display: contents;
+
+      a {
+        display: contents;
+      }
+
+      svg {
+        scale: 0.8;
+      }
+    }
+
+    .switchers {
+      display: flex;
+      gap: 16px;
+    }
+
+    @media screen and (min-width: 992px) {
+      height: 100px;
+
+      .logo {
+        svg {
+          scale: 1;
+        }
+      }
+    }
+  }
+}
+</style>
