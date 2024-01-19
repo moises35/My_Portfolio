9209ef17b6a2f79f2b4a24de6b8515f0633ac5dc
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2024-01-18 21:23:46 -0300

    Added: home section for mobile size

--- components/Section/Home.vue
+++ components/Section/Home.vue
@@ -1,20 +1,73 @@
 <template>
   <section class="home">
-    <div class="welcome" />
-    <div class="only-desktop img-container">
-      hola
+    <div class="presentation">
+      <p class="greeting">
+        ¡Hola! Mi nombre es,
+      </p>
+      <h1 class="name">
+        Moisés Alvarenga
+      </h1>
+      <p class="description">
+        Soy un estudiante de la carrera de Ingeniería en Informática de Paraguay.
+        Me encanta todo lo relacionado a técnologias de la web, tanto Frontend como Backend.
+        Estoy enfocado en construir aplicaciones con buena experiencia de usuario y código de alta calidad.
+      </p>
+      <div class="cta-container">
+        <AtomBtn
+          to="/"
+          text="CV"
+          :icon="SVGPdf"
+          :is-external="true"
+        />
+        <AtomBtn
+          to="/"
+          text="Contáctame"
+          :icon="SVGChat"
+          color="secondary"
+        />
+      </div>
+    </div>
+    <div class="only-desktop profile-container">
+      <!-- <img src="" alt="Moisés Alvarenga"> -->
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
+import SVGPdf from '@/assets/svg/pdf.svg'
+import SVGChat from '@/assets/svg/chat.svg'
 
 </script>
 
 <style lang="scss">
 section.home {
   display: flex;
+  align-items: center;
   height: 80vh;
-  background-color: tomato;
+
+  // background-color: rgb(255 99 71 / 9.3%);
+
+  .presentation {
+    .greeting {
+      margin-bottom: 6px;
+      font-size: 20px;
+      font-weight: 600;
+      color: var(--primary-color);
+    }
+
+    .name {
+      margin-bottom: 6px;
+      font-size: var(--big-title);
+    }
+
+    .description {
+      margin-bottom: 32px;
+    }
+
+    .cta-container {
+      display: flex;
+      gap: 22px;
+    }
+  }
 }
 </style>
