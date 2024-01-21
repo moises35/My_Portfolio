6a13bdceb123333b6215f0bfda21e7f88f7ab1ad
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2024-01-21 12:45:59 -0300

    Update: package.json and yarn.lock

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,38 @@
   <main>
     <SectionHome class="layout-wrapper" />
     <div class="divisor" />
+    <div class="layout-wrapper">
+      <SectionAboutMe />
+    </div>
+    <div class="layout-wrapper">
+      <SectionExperience />
+    </div>
+    <div class="layout-wrapper">
+      <SectionHabilities />
+      <SectionProjects />
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style lang="scss">
 main {
   .divisor {
     height: 10vh;
+    margin-bottom: 32px;
     background-color: var(--bg-secondary);
+  }
+
+  div.layout-wrapper {
+    margin-bottom: 32px;
+  }
+
+  @media screen and (min-width: 992px) {
+    .divisor {
+      height: 20vh;
+    }
   }
 }
 </style>
