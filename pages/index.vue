fb8053aa5e6f39178400c5cf6457ec2895310f3d
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2024-02-01 00:05:06 -0300

    Update layout and add contact section

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,20 @@
   <main>
     <SectionHome class="layout-wrapper" />
     <div class="divisor" />
-    <div class="layout-wrapper">
+    <div id="about-me" class="layout-wrapper">
       <SectionAboutMe />
     </div>
     <div class="layout-wrapper">
+      <SectionHabilities />
+    </div>
+    <div id="experience" class="layout-wrapper">
       <SectionExperience />
     </div>
-    <div class="layout-wrapper">
-      <SectionHabilities />
+    <div id="projects" class="layout-wrapper">
       <SectionProjects />
+    </div>
+    <div id="contacts" class="layout-wrapper">
+      <h1>Contactos nomass</h1>
     </div>
   </main>
 </template>
@@ -27,12 +32,12 @@ main {
   }
 
   div.layout-wrapper {
-    margin-bottom: 32px;
+    margin-bottom: 64px;
   }
 
   @media screen and (min-width: 992px) {
     .divisor {
-      height: 20vh;
+      height: 100px;
     }
   }
 }
